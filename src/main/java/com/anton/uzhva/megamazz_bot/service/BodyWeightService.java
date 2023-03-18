e9eaf1f3814e9b273f35c6c9fb6d9370035325f0
Anton Uzhva <anton.itec91@gmail.com>
2023-03-18 16:49:26 +0200

    Fixed some problems with getting body weight results

--- src/main/java/com/anton/uzhva/megamazz_bot/service/BodyWeightService.java
+++ src/main/java/com/anton/uzhva/megamazz_bot/service/BodyWeightService.java
@@ -52,17 +52,17 @@ public class BodyWeightService {
         if (daysToSubtract == 0) return false;
         BodyWeight theLatestRecord = (BodyWeight) getTheLatestRecord(chatId);
         Date theLatestRecordDate = theLatestRecord.created_at();
-        String firstQuery = "SELECT IF(COUNT(*)>0,1,0) FROM body_weight WHERE created_at BETWEEN :startDate - INTERVAL :daysToSubtract DAY AND :startDate";
-        String secondQuery = "SELECT IF(COUNT(*)>0,1,0) FROM body_weight WHERE created_at <:startDate - INTERVAL :daysToSubtract DAY AND :startDate";
-        BigInteger firstQueryResult = (BigInteger) getQueryResult(daysToSubtract, theLatestRecordDate, firstQuery, "startDate", "daysToSubtract");
-        BigInteger secondQueryResult = (BigInteger) getQueryResult(daysToSubtract, theLatestRecordDate, secondQuery, "startDate", "daysToSubtract");
+        String firstQuery = "SELECT IF(COUNT(*)>0,1,0) FROM body_weight WHERE created_at BETWEEN :startDate - INTERVAL :daysToSubtract DAY AND :startDate WHERE user_id=:id";
+        String secondQuery = "SELECT IF(COUNT(*)>0,1,0) FROM body_weight WHERE created_at <:startDate - INTERVAL :daysToSubtract DAY AND :startDate WHERE user_id=:id";
+        BigInteger firstQueryResult = (BigInteger) getQueryResult(chatId, daysToSubtract, theLatestRecordDate, firstQuery, "startDate", "daysToSubtract");
+        BigInteger secondQueryResult = (BigInteger) getQueryResult(chatId, daysToSubtract, theLatestRecordDate, secondQuery, "startDate", "daysToSubtract");
         int resultValue = firstQueryResult.add(secondQueryResult).intValue();
         return resultValue > 1;
     }
 
-
-    private Object getQueryResult(int daysToSubtract, Date theLatestRecordDate, String query, String... parameters) {
+    private Object getQueryResult(long chatId, int daysToSubtract, Date theLatestRecordDate, String query, String... parameters) {
         return entityManager.createNativeQuery(query)
+                .setParameter("id", chatId)
                 .setParameter(parameters[0], theLatestRecordDate)
                 .setParameter(parameters[1], daysToSubtract)
                 .getSingleResult();
@@ -74,17 +74,19 @@ public class BodyWeightService {
         Date theLatestRecordDate = theLatestRecord.created_at();
         String sqlQuery;
         if (daysToSubtract == 0 && period.getName().equals("Last result")) {
-            sqlQuery = "SELECT MAX(creared_at) FROM body_weight";
+            sqlQuery = "SELECT MAX(bw.crearedAt) FROM bodyWeight bw WHERE bw.user.id=:id";
             return entityManager.createNativeQuery(sqlQuery)
+                    .setParameter("id", chatId)
                     .getResultList();
         } else if (daysToSubtract == 0 && period.getName().equals("All results")) {
             sqlQuery = "FROM bodyWeight bw WHERE bw.user.id=:id";
             return entityManager.createQuery(sqlQuery)
-                    .setParameter("id",chatId)// TODO CHECK
+                    .setParameter("id", chatId)
                     .getResultList();
         } else {
-            sqlQuery = "SELECT * FROM body_weight WHERE created_at BETWEEN :startDate - INTERVAL :daysToSubtract DAY AND :startDate";
+            sqlQuery = "SELECT * FROM body_weight WHERE created_at BETWEEN :startDate - INTERVAL :daysToSubtract DAY AND :startDate AND user_id=:id";
             return entityManager.createNativeQuery(sqlQuery)
+                    .setParameter("id", chatId)
                     .setParameter("startDate", theLatestRecordDate)
                     .setParameter("daysToSubtract", daysToSubtract)
                     .getResultList();
