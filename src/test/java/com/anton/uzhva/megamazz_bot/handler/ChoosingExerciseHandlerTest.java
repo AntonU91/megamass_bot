ccac59e1202f7f5c120344f09b02d781eb3a0237
Anton Uzhva <anton.itec91@gmail.com>
2023-08-21 19:18:37 +0300

    --amend

---
+++ src/test/java/com/anton/uzhva/megamazz_bot/handler/ChoosingExerciseHandlerTest.java
@@ -0,0 +1,20 @@
+package com.anton.uzhva.megamazz_bot.handler;
+
+import com.anton.uzhva.megamazz_bot.service.UserService;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.shadow.com.univocity.parsers.common.input.LineSeparatorDetector;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChoosingExerciseHandlerTest {
+}
